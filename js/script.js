8e3d1a4a16ccbe595c06e4a3bfc3badc38557588
yarikdr <yarikdrapey@gmail.com>
2022-06-20 13:12:46 +0300

    works

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
+//header burger
 document.querySelector('.burger').addEventListener('click', () => {
     document.querySelector('.burger').classList.toggle('active');
     document.querySelector('.header__menu').classList.toggle('active');
+});
+
+// works
+document.querySelector('.works__btn').addEventListener('click', () => {
+    document.querySelector('.works__works').classList.toggle('view-all');
 });
