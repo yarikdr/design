d8fd63af20fcf0727c8f36407f16c52a5a71d018
yarikdr <yarikdrapey@gmail.com>
2022-06-20 14:25:27 +0300

    process

--- js/script.js
+++ js/script.js
@@ -8,3 +8,21 @@ document.querySelector('.burger').addEventListener('click', () => {
 document.querySelector('.works__btn').addEventListener('click', () => {
     document.querySelector('.works__works').classList.toggle('view-all');
 });
+
+//process 
+const video = document.querySelector('.process__video > video'),
+      pause = document.querySelector('.process__pause');
+
+let state = false;
+
+video.addEventListener('click', () => {
+    if (state) {
+        video.pause();
+        state = false;
+        pause.classList.remove('hide');
+    } else {
+        video.play();
+        state = true;
+        pause.classList.add('hide');
+    }
+});
